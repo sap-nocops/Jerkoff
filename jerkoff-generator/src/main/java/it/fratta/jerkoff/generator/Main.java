052a88b434eee568acdea6aa5c4bc0fc0a36656a
ElGansoSnowhiteDurden <rolando.luca88@gmail.com>
2017-03-31 13:02:42 +0200

    maven plugin wip

--- jerkoff-generator/src/main/java/it/fratta/jerkoff/generator/Main.java
+++ jerkoff-generator/src/main/java/it/fratta/jerkoff/generator/Main.java
@@ -4,12 +4,14 @@
 package it.fratta.jerkoff.generator;
 
 import java.io.File;
-import java.io.IOException;
+import java.util.List;
 import java.util.Properties;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 
 import com.google.common.reflect.ClassPath;
 import com.squareup.javapoet.TypeSpec;
@@ -24,9 +26,14 @@ import it.fratta.jerkoff.util.PropertiesUtils;
  * @author ElGansoSnowhiteDurden
  *
  */
-
 @Mojo(name = "generate-test")
 public class Main extends AbstractMojo {
+    
+    @Parameter( property = "generate-test.propertyfile", defaultValue = "${basedir}/src/main/resources/META-INF/generator.properties" )
+    private String propertyfile;
+    
+    @org.apache.maven.plugins.annotations.Component
+    private MavenProject project;
 
     /**
      * 
@@ -35,11 +42,12 @@ public class Main extends AbstractMojo {
     public void execute() throws MojoExecutionException {
         getLog().info("start main");
         try {
-            Properties prop = PropertiesUtils.loadProperties("META-INF/generator.properties");
+            getLog().info("load " + propertyfile);
+            Properties prop = PropertiesUtils.loadPropertiesFromAbs(propertyfile);
             String packageToScan = PropertiesUtils.getRequiredProperty(prop,
                     PropertiesUtils.PACKAGE_TO_SCAN);
             getLog().info("Scan package: " + packageToScan);
-            ClassPath classpath = ClassPath.from(Main.class.getClassLoader());
+            ClassPath classpath = ClassPath.from(getClassLoader());
             File sourcePath = new File(
                     PropertiesUtils.getRequiredProperty(prop, PropertiesUtils.TARGET_FOLDER));
             MongoDBDao mongo = new MongoDBDaoImpl(prop);
@@ -56,5 +64,25 @@ public class Main extends AbstractMojo {
             throw new MojoExecutionException(e.getMessage());
         }
         getLog().info("end main");
+    }
+
+    /**
+     * @return
+     */
+    private ClassLoader getClassLoader() {
+        //TODO
+//        DependencyResolutionRequiredException e;
+//        List<String> classpathElements = project.getCompileClasspathElements();
+//        List<URL> projectClasspathList = new ArrayList<URL>();
+//        for (String element : classpathElements) {
+//            try {
+//                projectClasspathList.add(new File(element).toURI().toURL());
+//            } catch (MalformedURLException e) {
+//                throw new MojoExecutionException(element + " is an invalid classpath element", e);
+//            }
+//        }
+//
+//        URLClassLoader loader = new URLClassLoader(projectClasspathList.toArray(new URL[0]));
+        return Main.class.getClassLoader();
     }
 }
--- jerkoff-lib/src/main/java/it/fratta/jerkoff/util/PropertiesUtils.java
+++ jerkoff-lib/src/main/java/it/fratta/jerkoff/util/PropertiesUtils.java
@@ -1,5 +1,6 @@
 package it.fratta.jerkoff.util;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -13,71 +14,95 @@ import com.google.common.base.Strings;
  */
 public class PropertiesUtils {
 
-	public static final String APP_NAME = "appName";
-	public static final String MONGO_DB = "mongo.database";
-	public static final String MONGO_HOST = "mongo.hostname";
-	public static final String MONGO_PORT = "mongo.port";
-	public static final String PACKAGE_TO_SCAN = "gen.packageToScan";
-	public static final String TARGET_FOLDER = "gen.targetFolder";
-	public static final String TEST_BASE_PACKAGE = "test.basePackage";
-	public static final String TEST_BASE_CLASS = "test.baseClass";
+    public static final String APP_NAME = "appName";
+    public static final String MONGO_DB = "mongo.database";
+    public static final String MONGO_HOST = "mongo.hostname";
+    public static final String MONGO_PORT = "mongo.port";
+    public static final String PACKAGE_TO_SCAN = "gen.packageToScan";
+    public static final String TARGET_FOLDER = "gen.targetFolder";
+    public static final String TEST_BASE_PACKAGE = "test.basePackage";
+    public static final String TEST_BASE_CLASS = "test.baseClass";
     public static final String LOG_DIR = "log4j.dir";
     public static final String LOG_PATTERN = "log4j.pattern";
     public static final String LOG_LEVEL = "log4j.level";
 
-	private PropertiesUtils() {
-		// empty constructor
-	}
+    private PropertiesUtils() {
+        // empty constructor
+    }
 
-	/**
-	 * 
-	 * @param propertyFileName
-	 * @return
-	 * @throws IOException
-	 */
-	public static Properties loadProperties(String propertyFileName) throws IOException {
-		InputStream propertiesIs = PropertiesUtils.class.getClassLoader().getResourceAsStream(propertyFileName);
-		Properties prop = new Properties();
-		prop.load(propertiesIs);
-		return prop;
-	}
+    /**
+     * 
+     * @param propertyFileName
+     * @return
+     * @throws IOException
+     */
+    public static Properties loadProperties(String propertyFileName) throws IOException {
+        InputStream propertiesIs = PropertiesUtils.class.getClassLoader()
+                .getResourceAsStream(propertyFileName);
+        return loadProperties(propertiesIs);
+    }
 
-	/**
-	 * 
-	 * @param prop
-	 * @param key
-	 * @return
-	 * @throws IllegalArgumentException if the property is missing
-	 */
-	public static String getRequiredProperty(Properties prop, String key) {
-		String p = getProperty(prop, key);
-		if (Strings.isNullOrEmpty(p)) {
-			throw new IllegalArgumentException("Property " + key + " is required.");
-		}
-		return p;
-	}
+    /**
+     * 
+     * @param absPropertyFileName
+     * @return
+     * @throws IOException
+     */
+    public static Properties loadPropertiesFromAbs(String absPropertyFileName) throws IOException {
+        InputStream propertiesIs = new FileInputStream(absPropertyFileName);
+        return loadProperties(propertiesIs);
+    }
 
-	/**
-	 * 
-	 * @param prop
-	 * @param key
-	 * @return
-	 */
-	public static String getProperty(Properties prop, String key) {
-		return prop.getProperty(key);
-	}
+    /**
+     * 
+     * @param propertiesIs
+     * @return
+     * @throws IOException
+     */
+    private static Properties loadProperties(InputStream propertiesIs) throws IOException {
+        Properties prop = new Properties();
+        prop.load(propertiesIs);
+        return prop;
+    }
 
-	/**
-	 * @param prop
-	 * @param mongoPort
-	 * @return
-	 * @throws IllegalArgumentException if the property is missing or not a number
-	 */
-	public static int getRequiredIntProperty(Properties prop, String key) {
-		try {
-			return Integer.parseInt(getRequiredProperty(prop, key));
-		} catch (NumberFormatException nfe) {
-			throw new IllegalArgumentException("Property " + key + " must be a number.");
-		}
-	}
+    /**
+     * 
+     * @param prop
+     * @param key
+     * @return
+     * @throws IllegalArgumentException
+     *             if the property is missing
+     */
+    public static String getRequiredProperty(Properties prop, String key) {
+        String p = getProperty(prop, key);
+        if (Strings.isNullOrEmpty(p)) {
+            throw new IllegalArgumentException("Property " + key + " is required.");
+        }
+        return p;
+    }
+
+    /**
+     * 
+     * @param prop
+     * @param key
+     * @return
+     */
+    public static String getProperty(Properties prop, String key) {
+        return prop.getProperty(key);
+    }
+
+    /**
+     * @param prop
+     * @param mongoPort
+     * @return
+     * @throws IllegalArgumentException
+     *             if the property is missing or not a number
+     */
+    public static int getRequiredIntProperty(Properties prop, String key) {
+        try {
+            return Integer.parseInt(getRequiredProperty(prop, key));
+        } catch (NumberFormatException nfe) {
+            throw new IllegalArgumentException("Property " + key + " must be a number.");
+        }
+    }
 }
